8610941813f32d29af77a6c538e57eaa77b96bfc
Ipsita Mishra <103277894+Meghaa702@users.noreply.github.com>
2024-09-18 00:43:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import base64
 
 st.set_page_config(
     page_title="VTS Customer Churn Prediction App",
-    page_icon="vts_logo1.png",  
+    page_icon="vts_logo1.png",
     layout='wide'
 )
 
@@ -33,7 +33,7 @@ credentials = {
 authenticator = stauth.Authenticate(
     credentials,
     'churn_app',  # app name
-    'abcdefg',  # key
+    'abcdefg',    # key
     10
 )
 
@@ -42,7 +42,6 @@ def get_base64_image(image_path):
         return base64.b64encode(img_file.read()).decode()
 
 image_path = 'vts_logo.png'
-
 img_base64 = get_base64_image(image_path)
 
 st.markdown('''
@@ -78,7 +77,6 @@ st.markdown('''
     <div class="welcome-container">
         <h1>Welcome to the VTS Customer Churn Prediction App</h1>
         <p>This application has been developed with dedication by Team Zigma.</p>
-        
     </div>
 ''', unsafe_allow_html=True)
 
@@ -92,11 +90,8 @@ with col1:
         st.session_state['authentication_status'] = authentication_status
         st.session_state['username'] = username
 
-        if authentication_status == None:
+        if authentication_status is None:
             st.info('Please Log in to continue')
-            # st.markdown('### **Demo credentials**')
-            # st.write('Username: user')
-            # st.write('Password: abc123')
             st.markdown('''
                 <style>
                 [data-testid="stSidebar"] {
@@ -109,8 +104,6 @@ if st.session_state.get('authentication_status') == False:
     with col1:
         st.title('Welcome to the VTS Customer Churn Prediction App')
         st.write('This application has been developed with dedication by Team Zigma.')
-        # st.write('See something you like? Leave us a star on [GitHub](https://github.com/Team-Zigma).⭐️😍')
-        # st.write('Connect with us on [LinkedIn](https://www.linkedin.com/in/team-zigma/) for further discussions.')
 
     with col2:
         st.error('Wrong username / password')
@@ -131,34 +124,37 @@ if st.session_state.get('authentication_status'):
     st.title(f'Heya {st.session_state["username"]}!!')
 
     st.markdown('''
-            <style>
-    .container p {
-        text-indent: 30px;  
-        text-align: justify; 
-    }
-    .container ul {
-        padding-left: 40px;  
-    }
-    .container li {
-        margin-bottom: 10px; 
-    }
-    </style>
-    <div class="container">
-    <h1>Welcome to the VTS Customer Churn Prediction App</h1>
-    <p>Our app leverages advanced machine learning algorithms to analyze customer data and identify patterns that indicate a high risk. Review and analyze past predictions, and gain a deeper understanding of your customers' behavior. Our intuitive design ensures a seamless and efficient experience. Simply input your customer data, select from our robust machine learning models (AdaBoost, Logistic Regression, or Random Forest), and get instant predictions on the likelihood of customer churn.</p>
+        <style>
+        .container p {
+            text-indent: 30px;
+            text-align: justify;
+        }
+        .container ul {
+            padding-left: 40px;
+        }
+        .container li {
+            margin-bottom: 10px;
+        }
+        </style>
+        <div class="container">
+        <h1>Welcome to the VTS Customer Churn Prediction App</h1>
+        <p>Our app leverages advanced machine learning algorithms to analyze customer data and identify patterns that indicate a high risk. Review and analyze past predictions, and gain a deeper understanding of your customers' behavior. Our intuitive design ensures a seamless and efficient experience. Simply input your customer data, select from our robust machine learning models (AdaBoost, Logistic Regression, or Random Forest), and get instant predictions on the likelihood of customer churn.</p>
 
-    <h2>Key Features</h2>
-    <ul>
-        <li><b>Machine Learning Models:</b> Choose from AdaBoost, Logistic Regression, or Random Forest models to find the best fit for your dataset.</li>
-        <li><b>Streamlined Prediction Process:</b> Get quick and accurate results with our efficient prediction process.</li>
-        <li><b>Data Retrieval:</b> Access your prediction history anytime, stored in a convenient "history.csv" file.</li>
-        <li><b>Data Display:</b> Easily review and analyze past predictions in a clear and organized tabular format.</li>
-    </ul>
+        <h2>Key Features</h2>
+        <ul>
+            <li><b>Machine Learning Models:</b> Choose from AdaBoost, Logistic Regression, or Random Forest models to find the best fit for your dataset.</li>
+            <li><b>Streamlined Prediction Process:</b> Get quick and accurate results with our efficient prediction process.</li>
+            <li><b>Data Retrieval:</b> Access your prediction history anytime, stored in a convenient "history.csv" file.</li>
+            <li><b>Data Display:</b> Easily review and analyze past predictions in a clear and organized tabular format.</li>
+        </ul>
 
-    <h1>GET STARTED TODAY!</h1>
-    <p>Explore our app and discover the value of accurate churn prediction.</p>
-    </div>
+        <h1>GET STARTED TODAY!</h1>
+        <p>Explore our app and discover the value of accurate churn prediction.</p>
+        </div>
     ''', unsafe_allow_html=True)
 
     st.markdown('### ================================================ ')
-    authenticator.logout('Logout', 'main')
+    try:
+        authenticator.logout('Logout', 'main')
+    except KeyError as e:
+        st.error(f"An error occurred during logout: {e}")
